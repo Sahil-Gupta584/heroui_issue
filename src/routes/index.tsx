b141b7c1dda5faabe71bf594e2672782bc5fb656
Sahil Gupta <guptas3067@gmail.com>
2025-08-29 17:26:48 +0530

    a

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,16 @@
-import { Button, HeroUIProvider } from '@heroui/react'
-import { createFileRoute } from '@tanstack/react-router'
+import { Button, HeroUIProvider } from "@heroui/react";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <HeroUIProvider>
-
-
-  <Button variant='bordered' color='primary' className='bg-red-50'>hi</Button>
+      <Button variant="bordered" color="primary" className="bg-red-50">
+        hi
+      </Button>
     </HeroUIProvider>
-  )
+  );
 }
